4042a51c7957e29eb8247ad093fb323465007005
himnynameisriz <v.rizwanali@gmail.com>
2018-07-29 13:05:19 -0500

    initial commit

---
+++ structs/structs.go
@@ -0,0 +1,15 @@
+package structs
+
+// Character class
+type Character struct {
+	Name     string
+	Age      float32
+	Playable bool
+
+	Strength     int
+	Constitution int
+	Dexterity    int
+	Intelligence int
+	Charisma     int
+	Wisdom       int
+}
